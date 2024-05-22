196b6bb70d33fdea523b54557e7ee680e7cbcbac
Vedant Sasane <vedant.sasane@wolkus.com>
2024-05-22 21:42:55 +0530

    Fixed Host

--- app/imports/client/pages/home/index.vue
+++ app/imports/client/pages/home/index.vue
@@ -51,7 +51,7 @@
         this.aiResponse = '';
         this.response = false;
         const startTime = +new Date();
-        const response = await fetch(`http://34.42.189.79:8000/generate-AI-response`, {
+        const response = await fetch(`${Meteor.settings.public.API_HOST}/generate-AI-response`, {
           method: 'POST',
           cache: 'no-cache',
           headers: {
@@ -61,13 +61,16 @@
         });
 
         this.aiResponse = await response.json();
-        const semanticSearch = await fetch(`http://34.42.189.79:8000/semantic-search?query=${queryText}&k=25`, {
-          method: 'POST',
-          cache: 'no-cache',
-          headers: {
-            'Content-Type': 'application/json',
+        const semanticSearch = await fetch(
+          `${Meteor.settings.public.API_HOST}/semantic-search?query=${queryText}&k=25`,
+          {
+            method: 'POST',
+            cache: 'no-cache',
+            headers: {
+              'Content-Type': 'application/json',
+            },
           },
-        });
+        );
 
         this.semanticSearchResponse = await semanticSearch.json();
         this.semanticSearchResponse.sort((a, b) => a.document.similarity_score - b.document.similarity_score);
